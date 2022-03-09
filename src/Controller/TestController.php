f858934f2b3754162935edcf383471f709f8778d
Prado <mahery.rsh@gmail.com>
2022-03-09 14:01:21 +0100

    parse xls data

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\Parser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class TestController extends AbstractController
 {
     #[Route('/test', name: 'test')]
-    public function index(): Response
+    public function index(Parser $parser): Response
     {
+        $res = $parser->parseXls( "test2");
+        dd($res);
+
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
---
+++ src/Service/Parser.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Service;
+
+use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+class Parser
+{
+    public function __construct(private ParameterBagInterface $parameters)
+    {
+    }
+
+    public function parseXls(string $filename): array
+    {
+        $res = [];
+
+        $baseRoute = $this->parameters->get('kernel.project_dir');
+        try {
+            $reader = new Xlsx();
+            $spreadsheet = $reader->load("$baseRoute/uploads/$filename.xlsx");
+
+            $data = $spreadsheet->getSheet(1)->toArray();
+            $dataLength = count($data);
+
+            // Get headers (Check if header is not located at first row)
+            $i = 0;
+            do {
+                $filters = array_filter($data[$i]);
+                $headers = $data[$i++];
+            } while (empty($filters));
+
+
+            // Combine headers & cell value to associative array
+            for ($row = $i; $row < $dataLength; $row++) {
+                $rowData = [];
+                foreach ($headers as $key => $header) {
+                    if (!is_null($header)) {
+                        $rowData[$header] = $data[$row][$key];
+                    }
+                }
+
+                if (!empty($rowData)) {
+                    $res[] = $rowData;
+                }
+            }
+
+        } catch (\Exception $e) {
+            $res = [
+                'error' => true,
+                'error_message' => $e->getMessage()
+            ];
+        }
+
+        return $res;
+    }
+}
